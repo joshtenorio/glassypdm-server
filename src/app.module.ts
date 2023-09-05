c2946e3c831c5b18c99c8e5370b057bbd05873ee
Joshua Tenorio <joshliam09@gmail.com>
2023-09-04 22:52:11 -0700

    minio mwe

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,26 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { IngestController } from './features/ingest/ingest.controller';
+import { S3Module } from 'nestjs-s3';
+import { IngestService } from './features/ingest/ingest.service';
+import { PrismaService } from './prisma/prisma.service';
+import { MinioModule } from '@svtslv/nestjs-minio';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    MinioModule.forRootAsync({
+      useFactory: () => ({
+        config: {
+          accessKey: process.env.S3_KEY_ID,
+          secretKey: process.env.S3_KEY_SECRET,
+          endPoint: process.env.S3_ENDPOINT,
+          port: 9000,
+          useSSL: false,
+        },
+      }),
+    })],
   controllers: [AppController, IngestController],
-  providers: [AppService],
+  providers: [AppService, IngestService, PrismaService],
 })
 export class AppModule {}
--- src/features/ingest/ingest.controller.ts
+++ src/features/ingest/ingest.controller.ts
@@ -1,13 +1,29 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Get, Body } from '@nestjs/common';
 import { FileMetadataDto } from './ingest.dto';
 import { IngestService } from './ingest.service';
+import { InjectS3, S3 } from 'nestjs-s3';
+import { InjectMinioClient, MinioClient } from '@svtslv/nestjs-minio';
 
 @Controller('ingest')
 export class IngestController {
-    constructor(private readonly ingestService: IngestService) {}
+    constructor(
+        private readonly ingestService: IngestService,
+        @InjectMinioClient() private readonly s3: MinioClient
+        ) {}
 
     @Post('metadata')
     async upload(@Body() metadata: FileMetadataDto): Promise<string> {
         return await this.ingestService.createFileRevision(metadata);
+    }
+
+    @Get('test-s3')
+    async listBuckets() {
+        try {
+            const list = await this.s3.listBuckets();
+            console.log(list);
+        } catch(e) {
+            console.log(e);
+        }
+        return "hehe";
     }
 }
