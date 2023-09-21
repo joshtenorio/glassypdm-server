6288a8d108a1adfd8a4efdd3188936b4bfa81931
Joshua Tenorio <joshliam09@gmail.com>
2023-09-20 22:04:32 -0700

    upload to s3 mwe

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,38 @@
 import express from "express";
 import cors from "cors";
 import { config } from "dotenv";
-//import mysql from "mysql2";
 
 const multer = require("multer");
 const mysql = require("mysql2");
-const upload = multer();
+const multerS3 = require("multer-s3");
+const { S3Client } = require("@aws-sdk/client-s3");
+
+config();
+
+const s3 = new S3Client({
+    region: process.env.S3_REGION,
+    credentials: {
+        secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+        accessKeyId: process.env.S3_ACCESS_KEY_ID
+    }
+})
+
+const upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: "glassy-pdm",
+        ACL: "public-read", // anyone w/ link can read
+        metadata: function(req: any, file: any, cb: any) {
+            cb(null, { fieldName: file.fieldname })
+        },
+        key: function(req: any, file: any, cb: any) {
+            cb(null, Date.now().toString());
+        }
+    })
+});
 
 const app = express();
 app.use(cors());
-
-config();
 
 const dbConnection = mysql.createConnection(process.env.DATABASE_URL);
 
@@ -18,21 +40,19 @@ dbConnection.connect();
 
 app.get("/", (req: any, res: any) => {
     console.log(req.body);
-    dbConnection.query('SELECT * FROM file', function (err: any, rows: any, fields: any) {
-        if (err) throw err
-    
-        res.send(rows)
-      })
+    res.send("nerd");
 });
 
-// upload.none() is text only multipart form
 app.post("/ingest", upload.single("key"), (req: any, res: any) => {
-    console.log("brrrr");
+    console.log("POST @ /ingest");
     console.log(req.body); //req.body["path"]
     console.log(req.file);
-    res.send("no");
+    console.log(req.file.location); // link to object
+    console.log(req.file.etag); // etag
+    console.log(req.file.key); // key (name of object)
+    res.send("bro");
 });
 
-app.listen(5000, () => {
-    console.log("listening on port 5000");
-})
+app.listen(process.env.PORT || 5000, () => {
+    console.log("listening on port ", process.env.PORT || 5000);
+});
